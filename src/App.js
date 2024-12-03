c07401f682fd56b27598332b3ef31b68f028c813
thhau1503 <thanhhaudeb204@gmail.com>
2024-12-03 09:58:02 +0700

    first deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import AdminLogin from "./pages/AdminLogin";
 import DashBoard from "./pages/DashBoard";
 import PaymentForm from "./pages/Order";
